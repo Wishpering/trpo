b46e52fe3e1455db2c015204b7444f58633ed96f
Wishpering <bani306@gmail.com>
2021-02-19 11:22:19 +0700

    разбил на несколько файлов

---
+++ lab1/src/utils/argparser.py
@@ -0,0 +1,11 @@
+from argparse import ArgumentParser, Namespace
+from sys import stderr
+
+class ModifiedArgumentParser(ArgumentParser):
+    def exit_with_error(self, error_text):
+        print(error_text, file=stderr)
+        exit(1)
+
+    def print_help_and_exit(self):
+        self.print_help()
+        exit(0)
