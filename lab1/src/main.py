c12f9486b61737db382cc53f92aae5e1bdc7eba1
Wishpering <bani306@gmail.com>
2021-02-19 11:08:13 +0700

    Фикс ошибок, связанных с subparser; ввод значений через input()

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -311,22 +311,22 @@ if __name__ == '__main__':
     scalar_operations.add_argument(
         '--sin',
         type=float,
-        help='Обратное',
+        help='Синус',
         dest='scal_sin')
     scalar_operations.add_argument(
         '--cos',
         type=float,
-        help='Обратное',
+        help='Косинус',
         dest='scal_cos')
     scalar_operations.add_argument(
         '--tan',
         type=float,
-        help='Обратное',
+        help='Тангенс',
         dest='scal_tan')
     scalar_operations.add_argument(
         '--ctg',
         type=float,
-        help='Обратное',
+        help='Котангенс',
         dest='scal_ctg')
 
     vector_operations = subparsers.add_parser(
@@ -334,59 +334,51 @@ if __name__ == '__main__':
     vector_operations.add_argument(
         '-s',
         '--sum',
-        nargs=2,
-        type=list,
+        action='store_true',
         help='Поэлементное сложение',
         dest='vec_sum')
     vector_operations.add_argument(
         '-m',
         '--mul',
-        nargs=2,
-        type=list,
+        action='store_true',
         help='Поэлементное умножение',
         dest='vec_mul')
     vector_operations.add_argument(
         '-sp',
         '--scalar',
-        nargs=2,
-        type=list,
+        action='store_true',
         help='Скалярное произведение',
         dest='vec_scal')
     vector_operations.add_argument(
         '-vm',
         '--vec_mul',
-        nargs=2,
-        type=list,
+        action='store_true',
         help='Векторное произведение',
         dest='vec_vecmul')
     vector_operations.add_argument(
         '-l',
         '--len',
-        type=list,
+        action='store_true',
         help='Длина вектора',
         dest='vec_len')
     vector_operations.add_argument(
         '--codirect',
-        nargs=2,
-        type=list,
+        action='store_true',
         help='Проверка сонаправленности векторов',
         dest='vec_cocheck')
     vector_operations.add_argument(
         '--is_ortog',
-        nargs=2,
-        type=list,
+        action='store_true',
         help='Проверка векторов на ортогональность',
         dest='vec_is_ortog')
     vector_operations.add_argument(
         '--mul_matr',
-        nargs=2,
-        type=list,
+        action='store_true',
         help='Умножение вектора на матрицу',
         dest='vec_on_matr')
     vector_operations.add_argument(
         '--mul_scal',
-        nargs=2,
-        type=list,
+        action='store_true',
         help='Умножение вектора на скаляр',
         dest='vec_on_scal')
 
@@ -394,115 +386,111 @@ if __name__ == '__main__':
         'matrix', help='Операции над матрицами')
     
     args = parser.parse_args()
-
-    dict_args = vars(args)
-    if len(dict_args) == 0:
-        parser.print_help_and_exit()
-    else:
-        any_set = False
-        
-        for i, j in dict_args.items():
-            if j != None:
-                any_set = True
-
-        if not any_set:
-            parser.print_help_and_exit()
             
     ########################
     #       Вектора
     #######################
     
     # Умножение вектора на скаляр
-    if args.vec_on_scal:
-        vec, scal = args.vec_on_scal
+    if getattr(args, 'vec_on_scal', False):
+        vec = input('Введите первый вектор, элементы вводятся через запятую:\n')
+        scal = float(input('Введите скаляр:\n'))
 
-        print(
-            Vector([float(i) for i in vec if i != ',']) *
-            Scalar(float(scal[0]))
-        )
+        result = Vector(
+            [float(i) for i in vec.split(',')]
+        ) * Scalar(scal)
+
+        print(result)
 
     # Поэлементное сложение векторов
-    elif args.vec_sum:
-        first_vec, second_vec = args.vec_sum
-
+    elif getattr(args, 'vec_sum', False):
+        first_vec = input('Введите первый вектор, элементы вводятся через запятую:\n')
+        second_vec = input('Введите второй вектор, элементы вводятся через запятую:\n')
+        
         print(
-            Vector([float(i) for i in first_vec if i != ',']) +
-            Vector([float(i) for i in second_vec if i != ','])
+            Vector([float(i) for i in first_vec.split(',')]) +
+            Vector([float(i) for i in second_vec.split(',')])
         )
 
     # Поэлементное умножение векторов
-    elif args.vec_mul:
-        first_vec, second_vec = args.vec_mul
+    elif getattr(args, 'vec_mul', False):
+        first_vec = input('Введите первый вектор, элементы вводятся через запятую:\n')
+        second_vec = input('Введите второй вектор, элементы вводятся через запятую:\n')
 
         print(
-            Vector([float(i) for i in first_vec if i != ',']) *
-            Vector([float(i) for i in second_vec if i != ','])
+            Vector([float(i) for i in first_vec.split(',')]) *
+            Vector([float(i) for i in second_vec.split(',')])
         )
 
     # Скалярное произведение векторов
-    elif args.vec_scal:
-        first_vec, second_vec = args.vec_scal
+    elif getattr(args, 'vec_scal', False):
+        first_vec = input('Введите первый вектор, элементы вводятся через запятую:\n')
+        second_vec = input('Введите второй вектор, элементы вводятся через запятую:\n')
 
         print(
             Vector(
-                [float(i) for i in first_vec if i != ',']
+                [float(i) for i in first_vec.split(',')]
             ).scalar_mul(
-                Vector([float(i) for i in second_vec if i != ','])
+                Vector([float(i) for i in second_vec.split(',')])
             )
         )
 
     # Векторное произведение трехмерных векторов
-    elif args.vec_vecmul:
-        first_vec, second_vec = args.vec_vecmul
+    elif getattr(args, 'vec_vecmul', False):
+        first_vec = input('Введите первый вектор, элементы вводятся через запятую:\n')
+        second_vec = input('Введите второй вектор, элементы вводятся через запятую:\n')
 
         try:
             print(
                 Vector(
-                    [float(i) for i in first_vec if i != ',']
+                    [float(i) for i in first_vec.split(',')]
                 ).vec_mul(
-                    Vector([float(i) for i in second_vec if i != ','])
+                    Vector([float(i) for i in second_vec.split(',')])
                 )
             )
         except ValueError:
-            parser.exit_with_error('Вектора должна быть трехмерными')
+            parser.exit_with_error('Оба вектора должны быть трехмерными')
 
     # Длина вектора
-    elif args.vec_len:
-        first_vec = args.vec_len
+    elif getattr(args, 'vec_len', False):
+        vector = input('Введите первый вектор, элементы вводятся через запятую:\n')
 
         print(
             Vector(
-                [float(i) for i in first_vec if i != ',']
+                [float(i) for i in vector.split(',')]
             ).vec_len
         )
 
     # Проверка сонаправленности векторов
-    elif args.vec_cocheck:
-        first_vec, second_vec = args.vec_cocheck
-
+    elif getattr(args, 'vec_cocheck', False):
+        first_vec = input('Введите первый вектор, элементы вводятся через запятую:\n')
+        second_vec = input('Введите второй вектор, элементы вводятся через запятую:\n')
+        
         print(
             Vector(
-                [float(i) for i in first_vec if i != ',']
+                [float(i) for i in first_vec.split(',')]
             ).is_collen(
-                Vector([float(i) for i in second_vec if i != ','])
+                Vector([float(i) for i in second_vec.split(',')])
             )
         )
 
     # Проверка векторов на ортогональность
-    elif args.vec_is_ortog:
-        first_vec, second_vec = args.vec_is_ortog
+    elif getattr(args, 'vec_is_ortog', False):
+        first_vec = input('Введите первый вектор, элементы вводятся через запятую:\n')
+        second_vec = input('Введите второй вектор, элементы вводятся через запятую:\n')
 
         print(
             Vector(
-                [float(i) for i in first_vec if i != ',']
+                [float(i) for i in first_vec.split(',')]
             ).is_ortog(
-                Vector([float(i) for i in second_vec if i != ','])
+                Vector([float(i) for i in second_vec.split(',')])
             )
         )
 
     # Умножение вектора на матрицу
-    elif args.vec_on_matr:
-        first_vec, input_matrix = args.vec_on_matr
+    elif getattr(args, 'vec_on_matr', False):
+        first_vec = input('Введите первый вектор, элементы вводятся через запятую:\n')
+        input_matrix = input('Введите матрицу, столбцы разделяются через ;, элементы - ,:\n')
 
         matrix = []
         tmp = []
@@ -520,63 +508,59 @@ if __name__ == '__main__':
 
         print(
             Vector(
-                [float(i) for i in first_vec if i != ',']
+                [float(i) for i in first_vec.split(',')]
             ).mul_on_matrix(
                 Matrix(matrix)
             )
         )
-    else:
-        parser.print_help_and_exit()
 
     ########################
     #       Скаляры
     #######################
 
     # Сумма скаляров
-    if args.scal_sum:
+    elif getattr(args, 'scal_sum', False):
         first_sc, second_sc = args.scal_sum
 
         print(Scalar(first_sc) + Scalar(second_sc))
 
     # Инверсия скаляра
-    elif args.scal_reverse:
+    elif getattr(args, 'scal_reverse', False):
         print(Scalar(args.scal_reverse).reverse)
 
     # Произведение скаляров
-    elif args.scal_mul:
+    elif getattr(args, 'scal_mul', False):
         first_sc, second_sc = args.scal_mul
 
         print(Scalar(first_sc) * Scalar(second_sc))
 
     # Возведение в степень скаляра
-    elif args.scal_pow:
+    elif getattr(args, 'scal_pow', False):
         first_sc, pow_val = args.scal_pow
 
         print(Scalar(first_sc).pow(pow_val))
 
     # Вычисление корня из скаляра
-    elif args.scal_sqrt:
+    elif getattr(args, 'scal_sqrt', False):
         first_sc, sqrt_val = args.scal_sqrt
 
         print(Scalar(first_sc).sqrt(sqrt_val))
 
     # Sin скаляра
-    elif args.scal_sin:
+    elif getattr(args, 'scal_sin', False):
         print(Scalar(args.scal_sin).sin)
 
     # Cos скаляра
-    elif args.scal_cos:
+    elif getattr(args, 'scal_cos', False):
         print(Scalar(args.scal_cos).cos)
 
     # Tan скаляра
-    elif args.scal_tan:
+    elif getattr(args, 'scal_tan', False):
         print(Scalar(args.scal_tan).tan)
 
     # Ctg скаляра
-    elif args.scal_ctg:
+    elif getattr(args, 'scal_ctg', False):
         print(Scalar(args.scal_ctg).ctg)
-    else:
-        parser.print_help_and_exit()
     
     #f = Matrix([[1, 2, 3], [4, 5, 6]])
     #s = Matrix([[1, 2], [4, 5], [3, 6]])
