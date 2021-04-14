d5bb2119536e4b93af19e08d3b33131b8d778bf5
Wishpering <bani306@gmail.com>
2021-04-14 10:20:36 +0700

    lab3 - more parallel

--- lab3/src/parallel.cpp
+++ lab3/src/parallel.cpp
@@ -3,6 +3,13 @@
 #include <omp.h>
 #include <random>
 
+float randFloat(double low, double high) {
+    thread_local static std::random_device rd;
+    thread_local static std::mt19937 rng(rd());
+    thread_local std::uniform_real_distribution<double> urd;
+    return urd(rng, decltype(urd)::param_type{low, high});
+}
+
 int main() {
   const size_t iteration_count = pow(10, 9),
     R = 4000;
@@ -10,19 +17,15 @@ int main() {
   size_t iteration_num = 0;
   uint64_t register X, Y;
   double chetcik;
-  
-  std::random_device rd;
-  std::mt19937_64 generator(rd());
-  std::uniform_real_distribution<> dist(0, R);
-    
+      
   omp_set_num_threads(16);
   omp_set_dynamic(true);
   
   #pragma omp parallel for private(iteration_num, X, Y) reduction(+ : chetcik)
-  for (iteration_num = 0; iteration_num < iteration_count; iteration_num++) {
-    X = dist(generator);
-    Y = dist(generator);
-
+  for (iteration_num = 0; iteration_num < iteration_count; iteration_num++) {    
+    X = randFloat(0, R);
+    Y = randFloat(0, R);
+      
     if ((X * X + Y * Y) <= R * R) chetcik++;
   }
 
